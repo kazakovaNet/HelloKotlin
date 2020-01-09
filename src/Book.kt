2e1777a3bfbf008ad65c64563105fde5a6f8098e
kazakovaNet <kazakova.net@yandex.ru>
2020-01-09 14:27:14 +0300

    intermediate commit

---
+++ src/Book.kt
@@ -0,0 +1,17 @@
+open class Book(val title: String, val author: String) {
+
+    var currentPage = 1
+
+    open fun readPage() {
+        currentPage++
+    }
+}
+
+class EBook(title: String, author: String, val format: String = "text") : Book(title, author) {
+
+    var wordsRead = 0
+
+    override fun readPage() {
+        wordsRead += 250
+    }
+}
